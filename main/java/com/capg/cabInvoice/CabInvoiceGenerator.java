5493178f4a52032a31855aff20ae458b732e7aa7
Jay002 <jayesh2598@gmail.com>
2020-10-21 11:12:45 +0530

    Start: Welcome message displayed.

---
+++ main/java/com/capg/cabInvoice/CabInvoiceGenerator.java
@@ -0,0 +1,8 @@
+package com.capg.cabInvoice;
+
+public class CabInvoiceGenerator {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Cab Invoice Generator Program!");
+	}
+}
